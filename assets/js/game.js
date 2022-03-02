8f428bb9fd8cc6a27ab2b25217f7a6f00231901d
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-02 14:07:34 +0000

    declared constants and added event listener for the easy button

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,61 @@
-// Setting up the html elements to place questions and answers
-
-const question = document.getElementById("question");
-
-//makes an array from the 4 answers buttons
-const answers = Array.from(document.getElementsByClassName("answer-text"));
-console.log(answers);
-
-
-
-
-
-
-// CALL API
-// declaring the api urls 
+// DECLARING CONSTANTS
 const easyQuiz = "https://opentdb.com/api.php?amount=15&difficulty=easy&type=multiple";
 const mediumQuiz = "https://opentdb.com/api.php?amount=15&difficulty=medium&type=multiple";
 const hardQuiz = "https://opentdb.com/api.php?amount=15&category=9&difficulty=hard&type=multiple";
+
+const easy = document.getElementById("easy");
+const medium = document.getElementById("medium")
+const hard = document.getElementById("hard")
+
+const question = document.getElementById("question");
+const next = document.getElementById("next");
+ 
+
+
+// CHOOSE DIFFICULTY - EVENT LISTENERS
+easy.addEventListener("click", alert("hello"));
+
+
+
+
+
+
+
+
+/*
+
+// START GAME FUNCTION
+startGame = () => {
+
+};
+
+
+
+
+
+function useApiData(data) {
+    document.getElementById("question").innerHTML = `${data.results[0].question}`
+}
+
+
+// START GAME
+//defines the start game function
+startGame = () => {
+    console.log("hello");
+
+async function easyApiRequest() {
+    let response = await fetch (easyQuiz);
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+};
+
+    document.getElementsByClassName("difficulty").classList.add("hide");
+    document.getElementByClass("quiz-area").classList.remove("hide");
+
+
+
+    useApiData(data)
+};
+
+*/
