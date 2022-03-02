d58069f7206748bd47e7bb620d7125b2691cd01b
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-02 22:44:46 +0000

    created an array that holds all the answers for a question, created function to shuffle the answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,18 +19,42 @@ function hideDifficulty() {
 }
 
 
-// START GAME FUNCTION
+// START GAME FUNCTION - calling api
 async function startGame() {
 const response = await fetch(easyQuiz);
 const data = await response.json();
 return data;
 };
 
+
+// SHUFFLE THE ANSWERS ARRAY - Fisher Yates Shuffle
+// shown on youtube tutorial - https://www.youtube.com/watch?v=eATLMjs7y4s&list=PL5egNEXQTWmFHAoWFVRLNAvD-9zzyWVxA&index=3
+function arrayShuffle(array) {
+for (let i = array.length -1; i > 0; i--) {
+    let s = Math.floor(Math.random() * (i + 1));
+[array[i], array[s]] = [array[s], array[i]];
+console.log(i);
+console.log(s);
+console.log(array);
+}
+};
+
+
+// Start Game Function - adding questions & answers
 startGame().then((data) => {
     console.log(data);
     const results = data.results[0];
+
+    // adds the question to the site
     document.getElementById("question").innerHTML = results.question;
 
+    // Create an array that holds all the answer choices for the question
+    const answers = [...results.incorrect_answers, results.correct_answer];
+    console.log(answers);
+
+
+    
+    arrayShuffle(answers);
 });
 
 
