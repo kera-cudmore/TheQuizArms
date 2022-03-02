40fb96eb03a9f120ac799a432a74dcc3d83a608e
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-02 09:34:49 +0000

    removed unneeded javascript from the file

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,56 +5,12 @@ const openModal = document.getElementById("open-modal");
 const closeModal = document.getElementById("close-modal");
 const modalContainer = document.getElementById("modal-container");
 
-
+// Open Modal
 openModal.addEventListener("click", () => {
     modalContainer.classList.add("show");
 });
 
+// Close Modal
 closeModal.addEventListener("click", () => {
     modalContainer.classList.remove("show");
 });
-
-
-// BUTTONS
-//index page play button
-
-const indexPlay = document.getElementById("play");
-
-indexPlay.onclick = function() {
-    window.location.href = "game.html";
-};
-
-// index page high scores button
-
-const indexHighScores = document.getElementById("highScores");
-
-indexHighScores.onclick = function() {
-    window.location.href = "highscores.html";
-};
-
-// high scores page play button
-
-const highScorePlay = document.getElementById("highScorePlay");
-
-highScorePlay.onclick = function() {
-    window.location.href="game.html";
-};
-
-
-// high scores page home button
-
-const highScoreHome = document.getElementById("highScoreHome");
-    
-highScoreHome.onclick = function() {
-    location.href = "index.html";
-};
-
-
-
-// CALL API
-
-// declaring the api urls 
-const easyQuiz = "https://opentdb.com/api.php?amount=15&difficulty=easy&type=multiple";
-const mediumQuiz = "https://opentdb.com/api.php?amount=15&difficulty=medium&type=multiple";
-const hardQuiz = "https://opentdb.com/api.php?amount=15&category=9&difficulty=hard&type=multiple";
-
