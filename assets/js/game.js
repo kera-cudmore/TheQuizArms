fb1fee7a8a706a2ecb5c3136bfaa97773f9e9a48
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-04 11:34:37 +0000

    added comments to sections, declared answer variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,34 +1,32 @@
 // DECLARING CONSTANTS
-const easyQuiz = "https://opentdb.com/api.php?amount=15&difficulty=easy&type=multiple";
-const mediumQuiz = "https://opentdb.com/api.php?amount=15&difficulty=medium&type=multiple";
-const hardQuiz = "https://opentdb.com/api.php?amount=15&category=9&difficulty=hard&type=multiple";
+// api
+const easy = "https://opentdb.com/api.php?amount=15&difficulty=easy&type=multiple";
+const medium = "https://opentdb.com/api.php?amount=15&difficulty=medium&type=multiple";
+const hard = "https://opentdb.com/api.php?amount=15&category=9&difficulty=hard&type=multiple";
 
+/* difficulty buttons
 const easy = document.getElementById("easy");
 const medium = document.getElementById("medium");
 const hard = document.getElementById("hard");
-
+*/
+//game page buttons
 const question = document.getElementById("question");
+const answer1 = document.getElementById("answer1")
+const answer2 =document.getElementById("answer2")
+const answer3 =document.getElementById("answer3")
+const answer4 =document.getElementById("answer4")
 const next = document.getElementById("next");
  
 
-// HIDE DIFFICULTY SECTION
+// HIDE DIFFICULTY SECTION FUNCTION
 function hideDifficulty() {
     document.getElementById("difficulty").classList.add("hide");
     document.getElementById("quiz-area").classList.remove("hide");
-
 }
 
 
-// START GAME FUNCTION - calling api
-async function startGame() {
-const response = await fetch(easyQuiz);
-const data = await response.json();
-return data;
-}
-
-
-// SHUFFLE THE ANSWERS ARRAY - Fisher Yates Shuffle
-// shown on youtube tutorial - https://www.youtube.com/watch?v=eATLMjs7y4s&list=PL5egNEXQTWmFHAoWFVRLNAvD-9zzyWVxA&index=3
+// SHUFFLE THE ANSWERS ARRAY FUNCTION - using Fisher Yates Shuffle
+// reseached on W3Schools and adapted as shown on youtube tutorial - https://www.youtube.com/watch?v=eATLMjs7y4s&list=PL5egNEXQTWmFHAoWFVRLNAvD-9zzyWVxA&index=3
 function arrayShuffle(array) {
 for (let i = array.length -1; i > 0; i--) {
     let s = Math.floor(Math.random() * (i + 1));
@@ -37,33 +35,40 @@ for (let i = array.length -1; i > 0; i--) {
 }
 
 
-// Start Game Function - adding questions & answers
-startGame().then((data) => {
-    console.log(data);
-    const results = data.results[0];
+// START GAME FUNCTION - calling api
+async function startGame() {
+    const response = await fetch(easyQuiz);
+    const data = await response.json();
 
-    // adds the question to the site
-    document.getElementById("question").innerHTML = results.question;
+        console.log(data);
+        const results = data.results[0];
+    
+        // adds the question to the site
+        document.getElementById("question").innerHTML = results.question;
+    
+        // Create an array that holds all the answer choices for the question
+        const answers = [...results.incorrect_answers, results.correct_answer];
+        console.log(answers);
+    
+        // answers array shuffled & added to answer buttons
+        arrayShuffle(answers);
+        console.log(answers);
+            answer1.innerHTML = `${answers[0]}`;
+            answer2.innerHTML = `${answers[1]}`;
+            answer3.innerHTML = `${answers[2]}`;
+            answer4.innerHTML = `${answers[3]}`;   
+        };
 
-    // Create an array that holds all the answer choices for the question
-    const answers = [...results.incorrect_answers, results.correct_answer];
-    console.log(answers);
 
-    // answers array shuffled
-    arrayShuffle(answers);
-    console.log(answers);
-
-        document.getElementById("answer1").innerHTML = `${answers[0]}`;
-        document.getElementById("answer2").innerHTML = `${answers[1]}`;
-        document.getElementById("answer3").innerHTML = `${answers[2]}`;
-        document.getElementById("answer4").innerHTML = `${answers[3]}`;
-
-
-});
-
-
-
-// CHOOSE DIFFICULTY - EVENT LISTENERS
+/* CHOOSE DIFFICULTY - EVENT LISTENERS
 // change this to the class of button difficulty so that it performs for all of the difficulty buttons
 easy.addEventListener("click", hideDifficulty);
 easy.addEventListener("click", startGame);
+*/
+
+const difficultyButtons = document.querySelectorAll(".difficulty")
+difficultyButtons.forEach(x => x.setAttribute("onclick", "hideDifficulty", "startGame(this.id)"))
+
+function callApi(theId){
+alert(theId)
+}
