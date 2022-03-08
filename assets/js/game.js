6bda5284a543a2ffd7e4100eed197d7ec50ca434
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-08 21:28:08 +0000

    added questionNo constant

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,14 +11,19 @@ const medium = document.getElementById("medium");
 const hard = document.getElementById("hard");
 
 //game page buttons
+let questionNo = 0;
+
+const questionNumber = document.getElementById("answer-no");
 const score = document.getElementById("score");
+
 const question = document.getElementById("question");
 const answer1 = document.getElementById("answer1");
 const answer2 = document.getElementById("answer2");
 const answer3 = document.getElementById("answer3");
 const answer4 = document.getElementById("answer4");
-const next = document.getElementById("next");
 const answerButtons = document.getElementsByClassName("answer-text");
+
+const next = document.getElementById("next");
 
 
 // HIDE DIFFICULTY SECTION FUNCTION
@@ -61,6 +66,7 @@ function increaseScore() {
 // NEXT QUESTION FUNCTION - run when next button pressed
 function nextQuestion(e) {
   console.log("next question");
+  getQuestion();
 }
 
 
@@ -75,7 +81,6 @@ function checkAnswer(e) {
     //add to the score counter
     increaseScore();
     // Remove the attribute on correct question ready for the next question
-    e.target.removeAttribute("data-correct", "true");
 
   } else {
         console.log("wrong answer");
@@ -86,49 +91,50 @@ function checkAnswer(e) {
     //removes hide class from the next button to display
     document.getElementById("next").classList.remove("hide");
     document.getElementById("next").addEventListener("click", nextQuestion);
-}
+    e.target.removeAttribute("data-correct", "true");
+  }
+
 
 
 // GET QUESTION FUNCTION
 function getQuestion(data) { 
-      document.getElementById("next").classList.add("hide");
+  document.getElementById("next").classList.add("hide");
 
-   
   let results = data.results[0];
 
+  if (questionNo <= 14) {
+    // adds the question to the site
+    document.getElementById("question").innerHTML = results.question;
 
-    if (results <= data.results[14]) {
+    const correctAnswer = results.correct_answer;
 
-      // adds the question to the site
-      document.getElementById("question").innerHTML = results.question;
+    // Create an array that holds all the answer choices for the question
+    const answers = [...results.incorrect_answers, correctAnswer];
 
-      const correctAnswer = results.correct_answer;
+    // answers array shuffled & added to answer buttons  (condensed using interation)
+     arrayShuffle(answers);
 
-      // Create an array that holds all the answer choices for the question
-      const answers = [...results.incorrect_answers, correctAnswer];
+    answer1.innerText = `${answers[0]}`;
+    answer2.innerText = `${answers[1]}`;
+    answer3.innerText = `${answers[2]}`;
+    answer4.innerText = `${answers[3]}`;
+    console.log(correctAnswer);
 
-      // answers array shuffled & added to answer buttons  (condensed using interation)
-      arrayShuffle(answers);
-
-      answer1.innerText = `${answers[0]}`;
-      answer2.innerText = `${answers[1]}`;
-      answer3.innerText = `${answers[2]}`;
-      answer4.innerText = `${answers[3]}`;
-      console.log(correctAnswer);
-
-      // loops through to check for correct answer & adds data attribute to the correct answer 
-      for (let button of answerButtons) {
-        if (button.innerText === correctAnswer) {
-          button.setAttribute("data-correct", "true");
-          console.log(button);
-        }
-        // adds event listener to each button & on click runs check answer function
-        button.addEventListener("click", checkAnswer);
+    // loops through to check for correct answer & adds data attribute to the correct answer 
+    for (let button of answerButtons) {
+      if (button.innerText === correctAnswer) {
+        button.setAttribute("data-correct", "true");
+        console.log(button);
       }
-    } else {
-      console.log("No more questions");
-      }
-}
+      // adds event listener to each button & on click runs check answer function
+      button.addEventListener("click", checkAnswer);
+    }
+  } else {
+    console.log("No more questions");
+    }
+    } 
+
+
 
 
 // CHOOSE DIFFICULTY - EVENT LISTENERS
