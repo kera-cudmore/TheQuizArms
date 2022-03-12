accfe22b167701a8ee7f9e68ed33f0c60c1ec9e0
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-12 23:40:17 +0000

    added missing semicolon suggested by jshint

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,5 +2,5 @@ const highScoresList = document.getElementById("highscoreslist");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores.map(highScores => {
-  return `<li class="highscoreitem">${highScores.name} - ${highScores.score}</li>`
+  return `<li class="highscoreitem">${highScores.name} - ${highScores.score}</li>`;
 }).join("");
