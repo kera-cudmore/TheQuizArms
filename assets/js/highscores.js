e8d038dbcbe9e73f0f7db1fc433f6d9d31e23ccb
Kera Cudmore <kera.cudmore@hotmail.com>
2022-03-14 09:06:14 +0000

    added comments to the code

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,6 +1,8 @@
+//High Score Constants
 const highScoresList = document.getElementById("highscoreslist");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+// adds to the high scores area
 highScoresList.innerHTML = highScores.map(highScores => {
   return `<li class="highscoreitem">${highScores.name} - ${highScores.score}</li>`;
 }).join("");
